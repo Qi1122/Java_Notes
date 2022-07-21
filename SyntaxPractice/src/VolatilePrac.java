3b1c421511707b761fbd0ecad92fa6e07e8f6918
Caroline Sun <carolinesun@Carolines-MBP.attlocal.net>
2022-07-20 20:14:13 -0500

    update

---
+++ SyntaxPractice/src/VolatilePrac.java
@@ -0,0 +1,8 @@
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class VolatilePrac {
+    public static void main(String[] args) {
+        List<Integer> list = new CopyOnWriteArrayList<>();
+    }
+}
